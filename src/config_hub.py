b317c04c4f95941d3d11e5d4d7f3822e046d5694
Zhongchao Qian <zqian@scripps.edu>
2021-03-04 10:56:38 -0800

    Update config_hub.py

--- src/config_hub.py
+++ src/config_hub.py
@@ -1,6 +1,16 @@
 import os
 
 
+DATA_SRC_MASTER_COLLECTION = 'src_master'             # for metadata of each src collections
+DATA_SRC_DUMP_COLLECTION = 'src_dump'                 # for src data download information
+DATA_SRC_BUILD_COLLECTION = 'src_build'               # for src data build information
+DATA_SRC_BUILD_CONFIG_COLLECTION = 'src_build_config' # for src data build configuration
+DATA_PLUGIN_COLLECTION = 'data_plugin'                # for data plugins information
+API_COLLECTION = 'api'                                # for api information (running under hub control)
+EVENT_COLLECTION = "event"
+CMD_COLLECTION = "cmd"
+
+
 # Max number of *processes* hub can access to run jobs
 HUB_MAX_WORKERS = int(os.cpu_count() / 4)
 
